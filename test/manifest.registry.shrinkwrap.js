a7f5602893287f9793d1952bb8cf8473c529df49
Kat Marchán <kzm@sykosomatic.org>
2016-12-07 01:40:00 -0800

    test: stubbed manifest shrinkwrap tests

---
+++ test/manifest.registry.shrinkwrap.js
@@ -0,0 +1,7 @@
+var test = require('tap').test
+
+test('fetches shrinkwrap data if missing + required')
+test('caches package data on shrinkwrap-related fetch')
+test('fails if shrinkwrap fetch failed + no caching')
+test('fails if shrinkwrap data fails to parse')
+test('fetches shrinkwrap data from existing local content')
