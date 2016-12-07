9f7add7021edc0edee7c48558a1308cc97956fe7
Kat Marchán <kzm@sykosomatic.org>
2016-12-06 23:51:34 -0800

    test: export utility fn to clear memoized registry manifest requests
    This should be generalized later

--- test/util/tnock.js
+++ test/util/tnock.js
@@ -1,8 +1,12 @@
 var nock = require('nock')
+var regManifest = require('../../lib/registry/manifest')
 
 module.exports = tnock
 function tnock (t, host) {
   var server = nock(host)
-  t.tearDown(function () { server.done() })
+  t.tearDown(function () {
+    server.done()
+    regManifest._clearMemoized()
+  })
   return server
 }
