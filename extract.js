45bb11fe1879075cc3ba86a9e5979332c43e4c7c
Kat Marchán <kzm@sykosomatic.org>
2017-02-02 19:29:35 -0800

    extract: use the extraction utility that was already written

--- extract.js
+++ extract.js
@@ -1,10 +1,8 @@
 var cache = require('./lib/cache')
-var gunzip = require('./lib/util/gunzip-maybe')
+var extractStream = require('./lib/util/extract-stream')
 var pipe = require('mississippi').pipe
-var pipeline = require('mississippi').pipeline
 var optCheck = require('./lib/util/opt-check')
 var rps = require('realize-package-specifier')
-var tar = require('tar-fs')
 
 module.exports = extract
 function extract (spec, dest, opts, cb) {
@@ -13,12 +11,7 @@ function extract (spec, dest, opts, cb) {
     opts = null
   }
   opts = optCheck(opts)
-  var xtractor = pipeline(gunzip(), tar.extract(dest, {
-    map: function (header) {
-      header.name = header.name.replace(/^package\//, '')
-      return header
-    }
-  }))
+  var xtractor = extractStream(dest, opts)
   var caStream = cache.get.stream.byDigest(opts.cache, opts.digest, opts)
   if (opts.digest) {
     opts.log.silly('extract', 'trying from digest:', opts.digest)
