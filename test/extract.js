13bec6c7d3379c526e0355a40293609ee9cc9d16
Kat Marchán <kzm@sykosomatic.org>
2017-02-02 18:39:32 -0800

    test: stubbed out extract-related tests

---
+++ test/extract.js
@@ -0,0 +1,7 @@
+var test = require('tap').test
+
+test('basic registry package extraction')
+test('basic git package extraction')
+test('basic hosted git package extraction')
+test('basic remote tarball package extraction')
+test('basic local tarball package extraction')
---
+++ test/registry.tarball.js
@@ -0,0 +1,3 @@
+var test = require('tap').test
+
+test('basic tarball streaming')
