14fd2a89566b771bf423b3b9e8ac0a0a478f38d9
Kat Marchán <kzm@sykosomatic.org>
2017-02-17 19:24:55 -0800

    fix(extract): fixed race condition
    Under heavy concurrency conditions, the original code would
    confuse the hell out of tar-fs, causing all sorts of random
    errors.
    
    This version is much clearer to read, and consing an
    extra extractStream basically costs nothing.

--- extract.js
+++ extract.js
@@ -13,23 +13,36 @@ function extract (spec, dest, opts, cb) {
     opts = null
   }
   opts = optCheck(opts)
-  var xtractor = extractStream(dest, opts)
-  var caStream = cache.get.stream.byDigest(opts.cache, opts.digest, opts)
   if (opts.digest) {
-    opts.log.silly('extract', 'trying from digest:', opts.digest)
-  }
-  caStream.on('error', function (err) {
-    if (err && err.code !== 'ENOENT') { return cb(err) }
-    rps(spec, function (err, res) {
-      if (err) { return cb(err) }
-      var tarball = require('./lib/handlers/' + res.type + '/tarball')
-      pipe(tarball(res, opts), xtractor, function (err) {
-        opts.log.silly('extract', 'extraction finished for', spec)
-        cb(err)
-      })
+    opts.log.silly('extract', 'trying ', spec, ' digest:', opts.digest)
+    extractByDigest(dest, opts, function (err) {
+      if (err && err === 'ENOENT') {
+        opts.log.silly('extract', 'digest for', spec, 'not present. Using manifest.')
+        return extractByManifest(spec, dest, opts, cb)
+      } else {
+        return cb(err)
+      }
     })
-  }).on('end', function () {
-    opts.log.silly('extract', spec, 'extracted by digest')
-    cb(null)
-  }).pipe(xtractor)
+  } else {
+    opts.log.silly('extract', 'no digest provided for ', spec, '- extracting by manifest')
+    extractByManifest(spec, dest, opts, cb)
+  }
+}
+
+function extractByDigest (dest, opts, cb) {
+  var xtractor = extractStream(dest, opts)
+  var cached = cache.get.stream.byDigest(opts.cache, opts.digest, opts)
+  pipe(cached, xtractor, cb)
+}
+
+function extractByManifest (spec, dest, opts, cb) {
+  var xtractor = extractStream(dest, opts)
+  rps(spec, function (err, res) {
+    if (err) { return cb(err) }
+    var tarball = require('./lib/handlers/' + res.type + '/tarball')
+    pipe(tarball(res, opts), xtractor, function (err) {
+      opts.log.silly('extract', 'extraction finished for', spec)
+      cb(err)
+    })
+  })
 }
