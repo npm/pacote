28fb412bc90972d2e0bed612ceccff235b06b455
Kat Marchán <kzm@sykosomatic.org>
2017-02-23 15:12:28 -0800

    refactor(tests): rename extract test to be registry-specific

--- test/extract.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-var test = require('tap').test
-
-test('basic registry package extraction')
-test('basic git package extraction')
-test('basic hosted git package extraction')
-test('basic remote tarball package extraction')
-test('basic local tarball package extraction')
---
+++ test/registry.extract.js
@@ -0,0 +1,5 @@
+'use strict'
+
+var test = require('tap').test
+
+test('basic registry package extraction')
