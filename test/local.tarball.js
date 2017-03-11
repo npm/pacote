e50d625d01a559a263a44e18c911c415d6083258
Kat Marchán <kzm@sykosomatic.org>
2017-03-11 00:47:44 -0800

    feat(local): local tarball support (#67)
    Fixes: #7

---
+++ test/local.tarball.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const BB = require('bluebird')
+
+const finished = BB.promisify(require('mississippi').finished)
+const fs = BB.promisifyAll(require('fs'))
+const mockTar = require('./util/mock-tarball')
+const npmlog = require('npmlog')
+const path = require('path')
+const test = require('tap').test
+
+const CACHE = require('./util/test-dir')(__filename)
+
+const tarball = require('../lib/handlers/local/tarball')
+
+npmlog.level = process.env.LOGLEVEL || 'silent'
+const OPTS = {
+  log: npmlog
+}
+
+test('basic tarball streaming', function (t) {
+  const pkg = {
+    'package.json': JSON.stringify({
+      name: 'foo',
+      version: '1.2.3'
+    }),
+    'index.js': 'console.log("hello world!")'
+  }
+  const tarballPath = path.join(CACHE, 'foo-1.2.3.tgz')
+  return mockTar(pkg).then(tarData => {
+    return fs.writeFileAsync(tarballPath, tarData).then(() => {
+      let data = ''
+      return finished(
+        tarball({
+          type: 'local',
+          spec: tarballPath
+        }, OPTS).on('data', d => { data += d })
+      ).then(() => {
+        t.equal(data, tarData, 'fetched tarball data matches one from local')
+      })
+    })
+  })
+})
