41852e0e9e09ac7f9ade3de6dbb2494e8a53af77
Kat Marchán <kzm@sykosomatic.org>
2017-06-29 11:03:39 -0700

    fix(extract): revert uid/gid change
    This reverts commit 6fc01a588b4974061ddf97fe94fd2aeaa0e15876.
    
    This was the wrong thing to do.
    
    BREAKING CHANGE: behavior for setting uid/gid on extracted contents was restored to what it was in pacote@2

---
+++ test/extract-stream.chown.js
@@ -0,0 +1,65 @@
+'use strict'
+
+const BB = require('bluebird')
+
+const fs = require('fs')
+const mockTar = require('./util/mock-tarball')
+const npmlog = require('npmlog')
+const path = require('path')
+const pipe = BB.promisify(require('mississippi').pipe)
+const requireInject = require('require-inject')
+const test = require('tap').test
+
+require('./util/test-dir')(__filename)
+
+npmlog.level = process.env.LOGLEVEL || 'silent'
+
+test('accepts gid and uid opts', {
+  skip: !process.getuid
+}, function (t) {
+  const pkg = {
+    'package.json': {
+      data: JSON.stringify({
+        name: 'foo',
+        version: '1.0.0'
+      })
+    },
+    'foo/index.js': 'console.log("hello world!")'
+  }
+  const NEWUID = process.getuid() + 1
+  const NEWGID = process.getgid() + 1
+  // All of this only happens on uid === 0
+  process.getuid = () => 0
+  const updatedPaths = []
+  const fsClone = Object.create(fs)
+  fsClone.chown = (p, uid, gid, cb) => {
+    process.nextTick(() => {
+      t.deepEqual({
+        uid: uid,
+        gid: gid
+      }, {
+        uid: NEWUID,
+        gid: NEWGID
+      }, 'correct owner set on ' + p)
+      updatedPaths.push(path.relative('.', p))
+      cb(null)
+    })
+  }
+  const extractStream = requireInject('../lib/extract-stream', {
+    fs: fsClone
+  })
+  return mockTar(pkg, {stream: true}).then(tarStream => {
+    return pipe(tarStream, extractStream('./target', {
+      uid: NEWUID,
+      gid: NEWGID,
+      log: npmlog
+    }))
+  }).then(() => {
+    t.deepEqual(updatedPaths, [
+      'target',
+      'target/package.json',
+      'target/foo',
+      'target/foo/index.js'
+    ], 'extracted files had correct uid/gid set')
+  })
+})
