6d58052f10460582db4da44621d3bec6d709482e
isaacs <i@izs.me>
2019-09-05 17:55:15 -0700

    test for process logger

--- test/util/proc-log.js
+++ test/util/proc-log.js
@@ -1,1 +1,11 @@
 const procLog = require('../../lib/util/proc-log.js')
+const t = require('tap')
+process.once('log', (...args) => t.same(args, ['warn', 1, 2, 3]))
+procLog.warn(1, 2, 3)
+t.same(Object.keys(procLog), [
+  'notice',  'error',
+  'warn',    'info',
+  'verbose', 'http',
+  'silly',   'pause',
+  'resume'
+])
