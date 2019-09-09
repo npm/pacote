72f15368c33cd12872150d303d8396983ace275b
isaacs <i@izs.me>
2019-09-08 20:51:33 -0700

    test for getting the HEAD sha

--- test/util/git/head-sha.js
+++ test/util/git/head-sha.js
@@ -1,1 +1,6 @@
 const headSha = require('../../../lib/util/git/head-sha.js')
+const t = require('tap')
+const {resolve} = require('path')
+const repo = resolve(__dirname, '../../..')
+
+t.resolveMatch(headSha(repo), /^[0-9a-f]{40}$/)
