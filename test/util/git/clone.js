8f1e8906890b2e9acfaedb91571b99fe97b82be9
isaacs <i@izs.me>
2019-09-09 10:13:28 -0700

    test for git.clone() shallow/full selector

--- test/util/git/clone.js
+++ test/util/git/clone.js
@@ -1,1 +1,12 @@
-const clone = require('../../../lib/util/git/clone.js')
+const shallow = require.resolve('../../../lib/util/git/shallow.js')
+const full = require.resolve('../../../lib/util/git/full.js')
+const requireInject = require('require-inject')
+const clone = requireInject('../../../lib/util/git/clone.js', {
+  [full]: () => 'full',
+  [shallow]: () => 'shallow',
+})
+
+const t = require('tap')
+t.equal(clone('git://github.com/foo/bar', '', '', '', 'ref'), 'shallow')
+t.equal(clone('git://other-host.com/foo/bar', '', '', '', 'ref'), 'full')
+t.equal(clone('git://github.com/foo/bar', '', '', '', null), 'full')
