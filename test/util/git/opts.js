bad55cdc6a99453c718ef1a2ab2c63efc110ef3f
isaacs <i@izs.me>
2019-12-04 12:28:19 -0800

    fix: Do not drop perms in git when not root
    Forward port of 5f3304028b6985fd380fc77c4840ff12a4898301
    
    Fix https://github.com/npm/cli/issues/476 for pacote v10.
    
    Fix https://github.com/npm/pacote/issues/22

--- test/util/git/opts.js
+++ test/util/git/opts.js
@@ -1,19 +1,43 @@
 const t = require('tap')
 const gitOpts = require('../../../lib/util/git/opts.js')
 const gitEnv = require('../../../lib/util/git/env.js')
-t.match(gitOpts({
-  foo: 'bar',
-  env: { override: 'for some reason' },
-}, {
-  uid: 420,
-  gid: 69,
-  abc: 'def',
-}), {
-  foo: 'bar',
-  env: { override: 'for some reason' },
-  uid: 420,
-  gid: 69,
-  abc: undefined,
-}, 'copied relevant opts, not irrelevant ones')
 
 t.match(gitOpts().env, gitEnv(), 'got the git env by default')
+
+t.test('as root', t => {
+  process.getuid = () => 0
+  t.match(gitOpts({
+    foo: 'bar',
+    env: { override: 'for some reason' },
+  }, {
+    uid: 420,
+    gid: 69,
+    abc: 'def',
+  }), {
+    foo: 'bar',
+    env: { override: 'for some reason' },
+    uid: 420,
+    gid: 69,
+    abc: undefined,
+  }, 'copied relevant opts, not irrelevant ones')
+  t.end()
+})
+
+t.test('as non-root', t => {
+  process.getuid = () => 999
+  t.match(gitOpts({
+    foo: 'bar',
+    env: { override: 'for some reason' },
+  }, {
+    uid: 420,
+    gid: 69,
+    abc: 'def',
+  }), {
+    foo: 'bar',
+    env: { override: 'for some reason' },
+    uid: undefined,
+    gid: undefined,
+    abc: undefined,
+  }, 'do not set uid/gid as non-root')
+  t.end()
+})
