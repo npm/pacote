9d7459440826ab4cf962ef98d8f3fd0c4d464b5c
isaacs <i@izs.me>
2021-02-18 13:30:58 -0800

    Do not crash on scp-style URLs for unknown hosts
    We can't use `new URL()` to parse these, because they're not valid URLs.
    But it turns out we don't have to actually parse the url just to swap
    out the hash portion anyway.
    
    Fix: #60
    
    PR-URL: https://github.com/npm/pacote/pull/66
    Credit: @isaacs
    Close: #66
    Reviewed-by: @nlf

---
+++ test/util/add-git-sha.js
@@ -0,0 +1,36 @@
+const t = require('tap')
+const addGitSha = require('../../lib/util/add-git-sha.js')
+const npa = require('npm-package-arg')
+
+const cases = [
+  // unknown host
+  ['git+ssh://git@some-host:user/repo', 'sha', 'git+ssh://git@some-host:user/repo#sha'],
+  ['git+ssh://git@some-host:user/repo#othersha', 'sha', 'git+ssh://git@some-host:user/repo#sha'],
+  ['git+ssh://git@some-host:user/repo#othersha#otherothersha', 'sha', 'git+ssh://git@some-host:user/repo#sha'],
+  ['git+ssh://git@some-host/user/repo', 'sha', 'git+ssh://git@some-host/user/repo#sha'],
+  ['git+ssh://git@some-host/user/repo#othersha', 'sha', 'git+ssh://git@some-host/user/repo#sha'],
+  ['git+ssh://git@some-host/user/repo#othersha#otherothersha', 'sha', 'git+ssh://git@some-host/user/repo#sha'],
+  // github shorthand
+  ['github:user/repo', 'sha', 'github:user/repo#sha'],
+  ['github:user/repo#othersha', 'sha', 'github:user/repo#sha'],
+  ['github:user/repo#othersha#otherothersha', 'sha', 'github:user/repo#sha'],
+  // github https with auth
+  ['git+https://git@github.com/user/repo', 'sha', 'https://git@github.com/user/repo.git#sha'],
+  ['git+https://git@github.com/user/repo#othersha', 'sha', 'https://git@github.com/user/repo.git#sha'],
+  ['git+https://git@github.com/user/repo#othersha#otherothersha', 'sha', 'https://git@github.com/user/repo.git#sha'],
+  // github https no auth
+  ['git+https://github.com/user/repo', 'sha', 'github:user/repo#sha'],
+  ['git+https://github.com/user/repo#othersha', 'sha', 'github:user/repo#sha'],
+  ['git+https://github.com/user/repo#othersha#otherothersha', 'sha', 'github:user/repo#sha'],
+  // github ssh
+  ['git+ssh://git@github.com/user/repo', 'sha', 'github:user/repo#sha'],
+  ['git+ssh://git@github.com/user/repo#othersha', 'sha', 'github:user/repo#sha'],
+  ['git+ssh://git@github.com/user/repo#othersha#otherothersha', 'sha', 'github:user/repo#sha'],
+  ['git+ssh://git@github.com:user/repo', 'sha', 'github:user/repo#sha'],
+  ['git+ssh://git@github.com:user/repo#othersha', 'sha', 'github:user/repo#sha'],
+  ['git+ssh://git@github.com:user/repo#othersha#otherothersha', 'sha', 'github:user/repo#sha'],
+]
+
+t.plan(cases.length)
+for (const [spec, sha, result] of cases)
+  t.equal(addGitSha(npa(spec), sha), result, `${spec} + ${sha} = ${result}`)
